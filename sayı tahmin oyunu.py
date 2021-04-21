ac17363e475f6027b2297753f927e1bee35ffc03
mstf-svndk <mustafff13@gmail.com>
2021-04-21 17:18:29 +0300

    Create sayı tahmin oyunu.py
    ss

---
+++ sayı tahmin oyunu.py
@@ -0,0 +1,41 @@
+import random
+import time
+
+print("""
+********************************************************************
+
+	  1 ile 100 arasındaki gizli sayıyı tahmin etme oyunu
+
+
+********************************************************************""")
+
+rasgele_sayı=random.randint(1,100)
+tahmin_hakkı=7
+kullanıcı=input("İsminizi Girin:")
+while True:
+	tahmin=int(input("Tahmin Giriniz:"))	
+	if tahmin<rasgele_sayı:
+		print("Bilgiler sorgulanıyor...")
+		time.sleep(1)
+
+		print("Daha yüksek bir sayı söyleyiniz...")
+
+		tahmin_hakkı-=1
+
+	elif tahmin>rasgele_sayı:
+		print("Bilgiler sorgulanıyor...")
+		time.sleep(1)
+
+		print("Daha düşük bir sayı söyleyiniz...")
+
+		tahmin_hakkı-=1
+	else:
+		print("Bilgiler sorgulanıyor...")
+		time.sleep(1)
+
+		print("Tebrikler",kullanıcı," gizli sayımız",rasgele_sayı)
+		break
+	if tahmin_hakkı==0:
+		print("Tahmin hakkınız bitti")
+		print(" gizli sayımız",rasgele_sayı)
+		break
